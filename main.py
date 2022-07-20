6756213a37f6712fc72392809a94ab5d2b82bc5e
Dmitry Shaplyko <shaplykodmitry@gmail.com>
2022-07-20 18:41:42 +0300

    Some fast changes

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def root():
     return render_template('index.html',
                            the_title="nbitell")
 
+@app.route('/website')
+def ab_website():
+    return "There will be about website section."
+
+@app.route('/cv')
+def vitae():
+    return "There will be my cv"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
